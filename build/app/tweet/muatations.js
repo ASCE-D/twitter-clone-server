bbbaf47a82ccac516666909481b2543e78978c0c
Deepesh Genani <72039675+ASCE-D@users.noreply.github.com>
2024-04-25 22:10:07 +0530

    Delete build directory

--- build/app/tweet/muatations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.muatations = void 0;
-exports.muatations = `#graphql
-    createTweet(payload: CreateTweetData!): Tweet
-    likeTweet(tweetId: ID!): Like
-    createComment(payload: CreateCommentData!): Comment
-`;
